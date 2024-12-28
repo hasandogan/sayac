f20fc5c4e5797439220bf42f22a4e0ff28eb9027
hasan-dogan <hasan.dogan@ideasoft.com.tr>
2024-12-28 20:09:23 +0300

    Initial Commit

---
+++ sayac/sayacApp.swift
@@ -0,0 +1,17 @@
+//
+//  sayacApp.swift
+//  sayac
+//
+//  Created by hasan doğan on 28.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct sayacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sayacTests/sayacTests.swift
@@ -0,0 +1,17 @@
+//
+//  sayacTests.swift
+//  sayacTests
+//
+//  Created by hasan doğan on 28.12.2024.
+//
+
+import Testing
+@testable import sayac
+
+struct sayacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
